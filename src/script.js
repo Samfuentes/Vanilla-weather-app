22a2c41064c46efe70fe5e9dc0bc0504600df540
SamFuentes <85533266+Samfuentes@users.noreply.github.com>
2022-04-15 15:23:55 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -59,7 +59,7 @@ function formateHour(timestamp) {
 }
 // Get Forecast
 function getForecast(coordinates) {
-  let apiKey = "940d67fee297ecd4e75bb56949c97896";
+  let apiKey = "9df8d23caa734224f75f20a4db0aee86";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -91,7 +91,7 @@ function displayCity(response) {
 }
 //Search city
 function searchCity(city) {
-  let apiKey = "940d67fee297ecd4e75bb56949c97896";
+  let apiKey = "9df8d23caa734224f75f20a4db0aee86";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayCity);
 }
